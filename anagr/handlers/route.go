efa672a3f894b14ea3bc07029c945acb21b31e75
icecube092 <icecube092@gmail.com>
2020-10-25 01:32:35 +0300

    -

--- anagr/handlers/route.go
+++ anagr/handlers/route.go
@@ -6,36 +6,90 @@ import (
 	"net/http"
 )
 
-
-type HttpHandler struct{
+// структура для обработки запросов на сервер
+type HttpHandler struct {
 	Storage map[string][]string
 }
 
-func (h *HttpHandler) ServeHTTP(response http.ResponseWriter, request *http.Request){
+func (h *HttpHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	defer request.Body.Close()
 	encoder := json.NewEncoder(response)
 	response.WriteHeader(200)
-	switch request.Method{
+	switch request.Method {
 	case "GET":
 		log.Println("GET\n", request)
 		words := getHandle(h, request)
-		err := encoder.Encode(words)
-		if err != nil{
+		err := encoder.Encode(words) // отдает массив строк
+		if err != nil {
 			log.Println(err)
 		}
 	case "POST":
 		log.Println("POST\n", request)
 		success := postHandle(h, request)
-		err := encoder.Encode(success)
-		if err != nil{
+		err := encoder.Encode(success) // отдает строку
+		if err != nil {
 			log.Println(err)
 		}
 	default:
 		log.Printf("%s request from %s\n", request.Method, request.RemoteAddr)
 		err := encoder.Encode("Only POST and GET method supported")
-		if err != nil{
+		if err != nil {
 			log.Println(err)
 		}
 	}
 
+}
+
+// обработчик GET запросов
+func getHandle(handler *HttpHandler, request *http.Request) []string {
+	if request.URL.Path != "/get" {
+		return []string{"Use '/get' path for GET request"}
+	}
+	if word := request.URL.Query().Get("word"); word != "" {
+		return getWordHandle(handler, word)
+	} else {
+		return []string{"Use 'word' key with not empty value for GET request"}
+	}
+}
+
+// обработчик POST запросов
+func postHandle(handler *HttpHandler, request *http.Request) string {
+	wordsChan := make(chan []string, 1)
+	if request.URL.RequestURI() != "/load" {
+		return "Use '/load' path for POST request"
+	}
+	if isValid(request, wordsChan) {
+		postWordHandle(handler, <-wordsChan)
+		return "Success"
+	} else {
+		return "Not valid request"
+	}
+}
+
+// валидация данных POST запроса
+func isValid(request *http.Request, channel chan []string) bool {
+	contentType := []string{"application/x-www-form-urlencoded"}
+	for index := range request.Header["Content-Type"] {
+		if request.Header["Content-Type"][index] != contentType[index] {
+			return false
+		}
+	}
+	err := request.ParseForm()
+	if err != nil {
+		return false
+	}
+	form := request.Form
+	var words []string
+	for array := range form {
+		err = json.Unmarshal([]byte(array), &words)
+		if err != nil {
+			return false
+		}
+		break
+	}
+	if len(words) == 0 {
+		return false
+	}
+	channel <- words
+	return true
 }
--- anagr/handlers/word_handle.go
+++ anagr/handlers/word_handle.go
@@ -4,62 +4,59 @@ import (
 	"strings"
 )
 
-func postWordHandle(handler *HttpHandler, words []string){
-	for _, word := range words{
-		found := false
+// обработка массива слов, полученных от POST запроса
+func postWordHandle(handler *HttpHandler, words []string) {
+	for _, word := range words {
+		wordFound := false
 		for keyWord, wordsArray := range handler.Storage {
-			match := true
-			if len(word) != len(keyWord){
+			if len(word) != len(keyWord) {
 				continue
 			}
-			for _, runeCode := range keyWord{
-				char := strings.ToLower(string(runeCode))
-				wordCount := strings.Count(strings.ToLower(word), char)
-				keyCount := strings.Count(strings.ToLower(keyWord), char)
-				if keyCount != wordCount{
-					match = false
-					break
-				}
-			}
-			if match{
-				found = true
-				hasDuplicate := false
-				for _, value := range wordsArray {
-					if word == value {
-						hasDuplicate = true
-						break
-					}
-				}
-				if !hasDuplicate {
+			if matchedWord := isAnagram(keyWord, word); matchedWord {
+				wordFound = true
+				if hasDuplicate := checkDuplicate(wordsArray, word); !hasDuplicate {
 					handler.Storage[keyWord] = append(handler.Storage[keyWord], word)
-
 				}
 			}
 		}
-		if !found{
+		if !wordFound {
 			handler.Storage[word] = []string{word}
 		}
 	}
 }
 
-func getWordHandle(handler *HttpHandler, getWord string)[]string{
-	for keyWord, _ := range handler.Storage {
-		match := true
-		if len(getWord) != len(keyWord){
+// поиск слов по GET запросу
+func getWordHandle(handler *HttpHandler, getWord string) []string {
+	for keyWord := range handler.Storage {
+		if len(getWord) != len(keyWord) {
 			continue
 		}
-			for _, runeCode := range keyWord{
-				char := strings.ToLower(string(runeCode))
-				wordCount := strings.Count(strings.ToLower(getWord), char)
-				keyCount := strings.Count(strings.ToLower(keyWord), char)
-				if keyCount != wordCount{
-					match = false
-					break
-				}
-			}
-			if match{
-				return handler.Storage[keyWord]
-			}
+		if matchedWord := isAnagram(keyWord, getWord); matchedWord {
+			return handler.Storage[keyWord]
 		}
+	}
 	return []string{}
+}
+
+// проверка слов на анаграмму
+func isAnagram(keyWord, word string) bool {
+	for _, runeCode := range keyWord {
+		char := strings.ToLower(string(runeCode))
+		wordCount := strings.Count(strings.ToLower(word), char)
+		keyCount := strings.Count(strings.ToLower(keyWord), char)
+		if keyCount != wordCount {
+			return false
+		}
+	}
+	return true
+}
+
+// проверка на дубликат
+func checkDuplicate(wordsArray []string, word string) bool {
+	for _, value := range wordsArray {
+		if word == value {
+			return true
+		}
+	}
+	return false
 }
